4d6d60fcca8b507a98bdf599a7252826ad376e0b
andre azu <andreazuaa@gmail.com>
2022-01-11 23:02:16 +0300

    add create profile

--- award_app/forms.py
+++ award_app/forms.py
@@ -1,8 +1,15 @@
-from django.forms import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile, Project, Rating
 
 class SignupForm(UserCreationForm):
     class Meta:
         model=User
         fields=["username", "email", "password1", "password2"]
+
+class CreateProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=["name", "projects"]
+        fields=["bio", "profile_pic", "contact"]
--- award_app/views.py
+++ award_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .forms import SignupForm
+from .forms import SignupForm,CreateProfile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
