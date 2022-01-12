406835efe6172a5d7dbc7b5967f8a2b35e68e09e
andre azu <andreazuaa@gmail.com>
2022-01-12 11:51:21 +0300

    add changes

--- award_project/settings.py
+++ award_project/settings.py
@@ -31,7 +31,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '',
+           'PORT': 5432,
        }
        
    }
@@ -116,14 +116,14 @@ WSGI_APPLICATION = 'award_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awardsapp',
-        'USER': 'andre',
-        'PASSWORD':'12345',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'awardsapp',
+#         'USER': 'andre',
+#         'PASSWORD':'12345',
+#     }
+# }
 
 
 # Password validation
