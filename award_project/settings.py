edab5bba40855f9d1fc77b82d4e6033b7deddfc7
andre azu <andreazuaa@gmail.com>
2022-01-12 12:22:08 +0300

    fix bug

--- award_project/settings.py
+++ award_project/settings.py
@@ -169,6 +169,8 @@ LOGIN_REDIRECT_URL="/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
+    '/var/www/static/',
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
